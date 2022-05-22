40b62daf6a7ec610c8bfab5e6c8ae4686b7cedf9
TahaGargourii <gargouriii.taha@gmail.com>
2022-05-22 01:11:26 +0100

    add paths and fixing bugs

--- projectManagement/src/main/java/com/management/cni/controller/AttachmentController.java
+++ projectManagement/src/main/java/com/management/cni/controller/AttachmentController.java
@@ -9,18 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/attachments")
 public class AttachmentController {
 
-/*  @Autowired
-  private com.management.cni.Service.AttachmentService attachmentService;
 
-  @GetMapping("/{idProject}")
-  public ResponseEntity<ApiResponse> getAllAttachmentsByProject(@PathVariable long idProject) {
-    ApiResponse apiResponse = attachmentService.g(idProject);
-    return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
-  }
-
-  @PostMapping("")
-  public ResponseEntity<ApiResponse> createAttachment(@RequestBody AttachmentRequest AttachmentRequest) {
-    ApiResponse apiResponse = attachmentService.createAttachment(AttachmentRequest);
-    return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
-  }*/
 }
